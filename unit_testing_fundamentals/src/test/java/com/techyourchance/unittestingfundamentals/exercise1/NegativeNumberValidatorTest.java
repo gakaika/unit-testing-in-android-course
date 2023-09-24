b97d8364480da466988a4126732f42a78c7229d0
Mayuresh Gaikwad <gaikwadm7776@gmail.com>
2023-09-23 21:53:51 -0400

    finish section 1

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,30 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
-public class NegativeNumberValidatorTest {
+import org.hamcrest.MatcherAssert;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Before;
+import org.junit.Test;
 
+public class NegativeNumberValidatorTest {
+    public NegativeNumberValidator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test1() {
+        MatcherAssert.assertThat(SUT.isNegative(-1), is(true));
+    }
+
+    @Test
+    public void test2() {
+        MatcherAssert.assertThat(SUT.isNegative(0), is(false));
+    }
+
+    @Test
+    public void test3() {
+        MatcherAssert.assertThat(SUT.isNegative(1), is(false));
+    }
 }
--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,31 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
-public class StringDuplicatorTest {
+import static org.hamcrest.CoreMatchers.is;
 
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringDuplicatorTest {
+    public StringDuplicator SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void stringDuplicator_emptyString_emptyStringReturned() {
+        MatcherAssert.assertThat(SUT.duplicate(""), is(""));
+    }
+
+    @Test
+    public void stringDuplicator_singleChar_charDuplicated() {
+        MatcherAssert.assertThat(SUT.duplicate("a"), is("aa"));
+    }
+
+    @Test
+    public void stringDuplicator_multipleChar_multipleCharDuplicated() {
+        MatcherAssert.assertThat(SUT.duplicate("abc"), is("abcabc"));
+    }
 }
